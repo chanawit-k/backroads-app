6778928ef4c928ab1930bd22b024c256898dec36
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2023-10-09 17:30:23 +0700

    - refactor footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,46 +1,36 @@
 import React from 'react'
-
+import { pageLink, socialLink } from '../data'
 const Footer = () => {
   return (
     <footer class="section footer">
       <ul class="footer-links">
-        <li>
-          <a href="#home" class="footer-link">
-            home
-          </a>
-        </li>
-        <li>
-          <a href="#about" class="footer-link">
-            about
-          </a>
-        </li>
-        <li>
-          <a href="#services" class="footer-link">
-            services
-          </a>
-        </li>
-        <li>
-          <a href="#featured" class="footer-link">
-            featured
-          </a>
-        </li>
+        {pageLink.map((link) => {
+          const { id, href, text } = link
+          return (
+            <li key={id}>
+              <a href={href} class="footer-link">
+                {text}
+              </a>
+            </li>
+          )
+        })}
       </ul>
       <ul class="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
-            <i class="fab fa-facebook"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
-            <i class="fab fa-twitter"></i>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" class="footer-icon">
-            <i class="fab fa-squarespace"></i>
-          </a>
-        </li>
+        {socialLink.map((link) => {
+          const { id, href, icon } = link
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noreferrer"
+                className="footer-icon"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          )
+        })}
       </ul>
       <p class="copyright">
         copyright &copy; Backroads travel tours company
